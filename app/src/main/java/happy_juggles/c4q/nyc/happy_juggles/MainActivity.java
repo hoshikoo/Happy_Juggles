ca3e9983e27ddaad2766d6beee58ee29bdd3f293
hoshikoo <hoshikooki@gmail.com>
2015-06-21 15:53:19 -0400

    Creating Lists and Cards

--- app/src/main/java/happy_juggles/c4q/nyc/happy_juggles/MainActivity.java
+++ app/src/main/java/happy_juggles/c4q/nyc/happy_juggles/MainActivity.java
@@ -1,19 +1,40 @@
 package happy_juggles.c4q.nyc.happy_juggles;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+//        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = {"one", "two", "three"} ;
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+//        mRecyclerView.setAdapter(mAdapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
