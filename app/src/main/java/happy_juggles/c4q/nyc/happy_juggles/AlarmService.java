83df097c8adaf3a8e8b50ac90106bb5486fa780f
hoshikoo <hoshikooki@gmail.com>
2015-07-01 21:53:45 -0400

    initial commit

---
+++ app/src/main/java/happy_juggles/c4q/nyc/happy_juggles/AlarmService.java
@@ -0,0 +1,24 @@
+package happy_juggles.c4q.nyc.happy_juggles;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Hoshiko on 7/1/15.
+ */
+public class AlarmService extends Service {
+
+    public static String TAG = AlarmService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        AlarmManagerHelper.setAlarms(this);
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
