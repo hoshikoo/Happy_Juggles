e2d78134b080ff2f794652f866b8993ab9057326
hoshikoo <hoshikooki@gmail.com>
2015-07-01 22:26:18 -0400

    removing alarm

--- app/src/main/java/happy_juggles/c4q/nyc/happy_juggles/AlarmManagerHelper.java
+++ app/src/main/java/happy_juggles/c4q/nyc/happy_juggles/AlarmManagerHelper.java
@@ -1,29 +1,29 @@
-package happy_juggles.c4q.nyc.happy_juggles;
-
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-/**
- * Created by Hoshiko on 7/1/15.
- */
-public class AlarmManagerHelper extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        setAlarms(context);
-    }
-
-    public static void setAlarms(Context context) {
-
-    }
-
-    public static void cancelAlarms(Context context) {
-
-    }
-
-    private static PendingIntent createPendingIntent(Context context, AlarmModel model) {
-        return null;
-    }
-}
+//package happy_juggles.c4q.nyc.happy_juggles;
+//
+//import android.app.PendingIntent;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//
+///**
+// * Created by Hoshiko on 7/1/15.
+// */
+//public class AlarmManagerHelper extends BroadcastReceiver {
+//
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        setAlarms(context);
+//    }
+//
+//    public static void setAlarms(Context context) {
+//
+//    }
+//
+//    public static void cancelAlarms(Context context) {
+//
+//    }
+//
+//    private static PendingIntent createPendingIntent(Context context, AlarmModel model) {
+//        return null;
+//    }
+//}
